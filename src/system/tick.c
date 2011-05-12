e68c12e6af89ec56fc48b268535a7eb4858ea796
Dario Susnisky <dario15th@gmail.com>
2011-05-12 17:44:47 -0300

    arreglada mi boludes en tick.c

--- src/system/tick.c
+++ src/system/tick.c
@@ -1,16 +1,8 @@
 #include "system/tick.h"
 #include "common.h"
-#include "library/stdio.h"
-#include "library/string.h"
 
 size_t ticksSinceStart = 0;
 
 void timerTick() {
     ticksSinceStart++;
-    char *original = "holaoa";
-    puts(strrchr(original,'o'));
-    
-    //putchar('a');
-    //putchar('\n');
-    while(1){}
 }
