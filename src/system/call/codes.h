9b49b350a0c1a6ee7f0db541cc860490756b3f65
acrespo <alvaro.andres.crespo@gmail.com>
2011-05-14 17:45:19 -0300

    OK now codes.h is added

---
+++ src/system/call/codes.h
@@ -0,0 +1,8 @@
+#ifndef     _system_call_codes_header_
+#define     _system_call_codes_header_
+
+#define     _SYS_READ       3
+#define     _SYS_WRITE      4
+#define     _SYS_IOCTL      54
+
+#endif
