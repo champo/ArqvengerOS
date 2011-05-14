09702c405015d67cf75d4b00e8d1fbd98ede4cef
Juan Pablo Civile <elementohb@gmail.com>
2011-05-13 23:24:05 -0300

    And we have a shell :D

--- src/system/kernel.c
+++ src/system/kernel.c
@@ -1,6 +1,6 @@
-#include "common.h"
 #include "system/interrupt.h"
 #include "system/call.h"
+#include "shell/shell.h"
 
 /**
  * Kernel entry point
@@ -11,11 +11,8 @@ void kmain() {
     write(1, "\033[1;1H\033[2J", 10);
 
     setupIDT();
-    char a[1];
     while (1) {
-        read(1, a, 1);
-        write(1, "b", 1);
-        halt();
+        shell();
     }
 }
 
