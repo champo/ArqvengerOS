8d976bf1c5bcb5377b972d34b08228b1965897d5
acrespo <alvaro.andres.crespo@gmail.com>
2011-05-28 20:22:54 -0300

    Ahora para calcular la CPU speed tomamos varias mediciones y hacemos un promedio. Still habria que probarlo en una compu, basta de BOCHS.

--- src/shell/date/date.c
+++ src/shell/date/date.c
@@ -6,12 +6,22 @@
 #include "system/rtc.h"
 #include "library/time.h"
 
+
+/**
+ * Command that shows the present date and current local time.
+ * 
+ * @param argv A string containing everything that came after the command.
+ */
 void date(char* argv) {
     
     char *str = asctime(NULL);
     printf("%s \n",str);
 }
 
+/**
+ * Print manual page for the date command.
+ * 
+ */
 void manDate(void) {
     setBold(1);
     printf("Usage:\n\tdate\n");
