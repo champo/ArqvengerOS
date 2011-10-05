5f608a2580228c18e232068e27f76eb5a0658919
Juan Pablo Civile <elementohb@gmail.com>
2011-10-05 00:15:45 -0300

    No need for intermediate variables

--- src/system/scheduler.c
+++ src/system/scheduler.c
@@ -7,14 +7,14 @@ struct ProcessQueue scheduler_queue = {.first = NULL, .last = NULL};
 
 struct Process* scheduler_curr = NULL;
 
-static union longlong { 
-    struct timeStampCounte { int low; int high; } tsc; 
-    unsigned long long val; 
+union longlong {
+    struct timeStampCounte { int low; int high; } tsc;
+    unsigned long long val;
 };
 
 static unsigned long long cycles = 0;
 
-static void update_cycles();
+static void update_cycles(void);
 
 void scheduler_add(struct Process* process) {
     process_queue_push(&scheduler_queue, process);
@@ -24,7 +24,7 @@ void scheduler_do(void) {
 
     if (scheduler_curr != NULL) {
         __asm__ __volatile ("mov %%ebp, %0":"=r"(scheduler_curr->mm.esp)::);
-        update_cycles(); 
+        update_cycles();
     }
 
     choose_next();
@@ -55,17 +55,15 @@ unsigned long long scheduler_get_cycles(void) {
 }
 
 
-void update_cycles() {
-    int low,high;
+void update_cycles(void) {
+
     union longlong a;
     unsigned long long newCycles;
-    
-    __asm__ __volatile__ ("rdtsc; mov %%eax, %0; mov %%edx, %1": "=a"(low), "=d"(high)); 
-    
-    a.tsc.low = low;
-    a.tsc.high = high;
+
+    __asm__ __volatile__ ("rdtsc; mov %%eax, %0; mov %%edx, %1": "=a"(a.tsc.low), "=d"(a.tsc.high));
+
     newCycles = a.val;
-    
+
     scheduler_curr->cycles += (newCycles - cycles);
     cycles = newCycles;
 }
