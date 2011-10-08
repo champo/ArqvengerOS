3097e76234b796bf0434e377a7ba690af4586ccd
Juan Pablo Civile <elementohb@gmail.com>
2011-10-08 17:24:35 -0300

    Remove the functions and add the block group structure

--- src/drivers/ext2/blockGroup.h
+++ src/drivers/ext2/blockGroup.h
@@ -1,15 +1,14 @@
-#ifndef __BLOCK_GROUP__
-#define __BLOCK_GROUP__
+#ifndef __EXT2_BLOCK_GROUP__
+#define __EXT2_BLOCK_GROUP__
 
-/*
-int block_group_descriptor_init(void);
-int get_block_group_descriptor(int index, void* blockGroupDescriptor);
-int block_group_get_adress_block_usage(void* blockGroupDescriptor);
-int block_group_get_adress_inode_usage(void* blockGroupDescriptor);
-int block_group_get_start_block_adress_inode_table(void* blockGroupDescriptor);
-int block_group_get_total_unallocated_blocks(void* blockGroupDescriptor);
-int block_group_get_total_unallocated_inodes(void* blockGroupDescriptor);
-int block_group_get_total_directories(void* blockGroupDescriptor);
-*/
+struct BlockGroupDescriptor {
+    unsigned int blockBitmapAddress;
+    unsigned int inodeBitmapAddress;
+    unsigned int inodeTableStart;
+    unsigned short unallocatedBlocks;
+    unsigned short unalloactedInodes;
+    unsigned short directoryCount;
+    unsigned char padding[14];
+};
 
 #endif
