0c9870b69287ded634e18543035a6e7e05fd84b3
Dario Susnisky <dario15th@gmail.com>
2011-10-08 19:36:28 -0300

    starting inode

---
+++ src/drivers/ext2/inode.h
@@ -0,0 +1,30 @@
+#ifndef __EXT2_INODE__
+#define __EXT2_INODE__
+
+#include "type.h"
+
+struct ext2_Inode {
+    word typesAndPermissions;
+    word userID;
+    dword lowerSize;
+    dword lastAccess; //Posix time.
+    dword creationTime; //Posix time.
+    dword lastModification; //Posix time.
+    dword deletitionTime; //Posix time.
+    word groupID;
+    word hardLinks;
+    dword counDiskSectors;
+    dword flags;
+    dword operatingSystemValue;
+    dword directBlockPointers[12];
+    dword singlyIndirectBlockPointer;
+    dword doublyIndirectBlockPointer;
+    dword triplyIndirectBlockPointer;
+    dword generationNumber;
+    dword reserverd1;
+    dword reserved2;
+    dword blockAddressOfFragment;
+    dword operatingSystemValue2[3];
+};
+
+#endif
