e0ccdc64b93cbd72e3e9fcf4d91e76c4cd42a578
Juan Civile <elementohb@gmail.com>
2011-10-16 21:17:33 -0300

    Clean up the test code

--- src/drivers/ext2/ext2.c
+++ src/drivers/ext2/ext2.c
@@ -41,44 +41,6 @@ struct ext2* ext2_load(unsigned long long startSector) {
 
     ext2_read_blockgroup_table(fs);
 
-    kprintf("Block size %u Sectors per block %u Blocks per group %u\n", fs->blockSize, fs->sectorsPerBlock, fs->sb->blocksPerBlockGroup, fs->sb->inodesPerBlockGroup);
-    for (size_t i = 0; i < fs->blockGroupCount; i++) {
-        kprintf("Group %d: free blocks %u, table start %u, bitmap %u\n", i, fs->groupTable[i].unallocatedBlocks, fs->groupTable[i].inodeTableStart,
-                fs->groupTable[i].blockBitmapAddress);
-    }
-
-    struct fs_Directory* dir = kalloc(sizeof(struct fs_Directory));
-    dir->offset = 0;
-    dir->inode = ext2_read_inode(fs, 2);
-    kprintf("Root dir size %u type %u\n", dir->inode->data->size, INODE_TYPE(dir->inode->data));
-    struct DirectoryEntry entry = ext2_dir_read(dir);
-    while (entry.inode != 0) {
-        dir->offset += entry.entryLength;
-        kprintf("Entry %s(%u) inode %u\n", entry.name, entry.nameLength, entry.inode);
-        entry = ext2_dir_read(dir);
-    }
-
-    dir->inode = ext2_read_inode(fs, 16);
-    kprintf("test dir size %u type %u\n", dir->inode->data->size, INODE_TYPE(dir->inode->data));
-    dir->offset = 0;
-    entry = ext2_dir_read(dir);
-    size_t lastNode;
-    while (entry.inode != 0) {
-        dir->offset += entry.entryLength;
-        kprintf("Entry %s(%u) inode %u\n", entry.name, entry.nameLength, entry.inode);
-        lastNode = entry.inode;
-        entry = ext2_dir_read(dir);
-    }
-
-    struct fs_Inode* plist = ext2_read_inode(fs, lastNode);
-    char* buff = kalloc(1200);
-    ext2_read_inode_content(plist, 0, 4, buff);
-    kprintf("%u %u %u %u\n", buff[0], buff[1], buff[2], buff[3]);
-    kprintf("%u\n", plist->data->size);
-    ext2_read_inode_content(plist, 0, 1200, buff);
-    for (int i = 0; i < 1200; i++) {
-        if (buff[i] != '0' + (i % 10)) kprintf("CRAP");
-    }
     return fs;
 }
 
