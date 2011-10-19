0417d4d7754efbe8a1e05952ac2e4559e5889a87
Juan Pablo Civile <elementohb@gmail.com>
2011-10-19 02:10:34 -0300

    Fill in some of the op types

--- src/drivers/ext2/ext2.c
+++ src/drivers/ext2/ext2.c
@@ -2,7 +2,18 @@
 #include "drivers/ext2/internal.h"
 #include "drivers/ext2/superblock.h"
 #include "drivers/ext2/blockGroup.h"
+#include "drivers/ext2/inode.h"
+#include "drivers/ext2/directory.h"
+#include "system/fs/fs.h"
 #include "system/mm.h"
+
+static size_t inode_write(struct FileDescriptor* fd, const void* buffer, size_t len);
+
+static size_t inode_read(struct FileDescriptor* fd, void* buffer, size_t len);
+
+static struct fs_DirectoryEntry readdir(struct FileDescriptor* fd);
+
+static struct fs_DirectoryEntry findentry(struct FileDescriptor* fd, const char* name);
 
 struct ext2* ext2_load(unsigned long long startSector) {
 
@@ -38,6 +49,87 @@ struct ext2* ext2_load(unsigned long long startSector) {
 
     ext2_read_blockgroup_table(fs);
 
+    fs_register_ops(INODE_DIR, (struct FileDescriptorOps) {
+            .write = NULL,
+            .read = NULL,
+            .ioctl = NULL,
+            .readdir = readdir,
+            .findentry = findentry,
+            .close = NULL
+    });
+
+    struct FileDescriptorOps fileOps = {
+        .write = inode_write,
+        .read = inode_read,
+        .ioctl = NULL,
+        .readdir = NULL,
+        .findentry = NULL,
+        .close = NULL
+    };
+
+    fs_register_ops(INODE_FILE, fileOps);
+    fs_register_ops(INODE_LINK, fileOps);
+
     return fs;
+}
+
+size_t inode_write(struct FileDescriptor* fd, const void* buffer, size_t len) {
+
+    int res = ext2_write_inode_content(fd->inode, fd->offset, len, buffer);
+    if (res == -1) {
+        return -1;
+    } else {
+        fd->offset += res;
+        return res;
+    }
+}
+
+size_t inode_read(struct FileDescriptor* fd, void* buffer, size_t len) {
+    int res = ext2_read_inode_content(fd->inode, fd->offset, len, buffer);
+    if (res == -1) {
+        return -1;
+    } else {
+        fd->offset += res;
+        return res;
+    }
+}
+
+struct fs_DirectoryEntry readdir(struct FileDescriptor* fd) {
+    struct DirectoryEntry entry;
+    do {
+        entry = ext2_dir_read(fd->inode, fd->offset);
+        fd->offset += entry.entryLength;
+    } while (entry.inode == 0);
+
+    if (entry.entryLength == 0) {
+        return (struct fs_DirectoryEntry) {
+            .inode = 0,
+            .length = 0
+        };
+    }
+
+    struct fs_DirectoryEntry res = {
+        .inode = entry.inode,
+        .length = entry.nameLength
+    };
+
+    strcpy(res.name, entry.name);
+
+    return res;
+}
+
+struct fs_DirectoryEntry findentry(struct FileDescriptor* fd, const char* name) {
+    struct DirectoryEntry entry = ext2_dir_find(fd->inode, name);
+    struct fs_DirectoryEntry res;
+
+    if (entry.entryLength) {
+        res.inode = entry.inode;
+        res.length = entry.nameLength;
+        strcpy(res.name, entry.name);
+    } else {
+        res.inode = 0;
+    }
+
+    return res;
 }
 
