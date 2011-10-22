f2448494bec8e7f65686cf56b405ced2304ff809
Juan Civile <elementohb@gmail.com>
2011-10-22 12:20:54 -0300

    Disable interrupts until we actually need them close #32

--- src/system/kernel.c
+++ src/system/kernel.c
@@ -41,7 +41,6 @@ void kmain(struct multiboot_info* info, unsigned int magic) {
     ata_init(info);
     scheduler_init();
 
-    disableInterrupts();
     struct Process* idleProcess = process_table_new(idle, NULL, NULL, 1, NO_TERMINAL, 0);
     struct Process* shellProcess = process_table_new(tty_run, NULL, idleProcess, 1, NO_TERMINAL, 0);
     enableInterrupts();
