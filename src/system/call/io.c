8daee5f66614d1f649f624fb9acf6018a9964b77
Juan Civile <elementohb@gmail.com>
2011-10-23 21:39:43 -0300

    Refactor open and creat

--- src/system/call/io.c
+++ src/system/call/io.c
@@ -10,6 +10,12 @@
 #include "library/string.h"
 #include "constants.h"
 
+static struct fs_Inode* resolve_path(const char* path);
+
+static char* path_directory(const char* path);
+
+static char* path_file(const char* path);
+
 size_t _read(int fd, void* buf, size_t length) {
 
     struct Process* process = scheduler_current();
@@ -43,18 +49,11 @@ size_t _write(int fd, const void* buf, size_t length) {
 }
 
 int _open(const char* path, int flags, int mode) {
-    struct fs_Inode* curdir;
-    struct fs_DirectoryEntry nextdir;
-    int index = 0;
-    char entry[ENTRY_NAME_MAX_LEN];
-    int len = strlen(path);
-    int gid, uid, file_gid, file_uid;
-    int fd = -1;
-    int i = 0;
 
     struct Process* process = scheduler_current();
+    int fd = -1;
 
-    for (i = 0; i < MAX_OPEN_FILES; i++) {
+    for (size_t i = 0; i < MAX_OPEN_FILES; i++) {
         if (process->fdTable[i].inode == NULL) {
             fd = i;
             break;
@@ -65,57 +64,62 @@ int _open(const char* path, int flags, int mode) {
         return -1;
     }
 
-    if (path[index] == '/') {
-        curdir = fs_root();
-        index++;
-    } else {
-        //TODO curdir = getcwd();
+    char* base = path_directory(path);
+    kprintf("Trying to open %s", base);
+    char* filename = path_file(path);
+    kprintf(" %s\n", filename);
+
+    struct fs_Inode* directory = resolve_path(base);
+    if (directory == NULL) {
+        kfree(base);
+        kfree(filename);
+        return -1;
     }
 
-    for (int i = 0; index < len; index++) {
 
-        entry[i] = path[index];
-        i++;
+    struct fs_DirectoryEntry fileEntry = fs_findentry(directory, filename);
+    if (fileEntry.inode == 0) {
 
-        if (path[index] == '/' || index + 1 == len) {
-            if (index + 1 != len) {
-                i--;
-            }
+        if (!(flags & O_CREAT) || _creat(path, mode) != 0) {
+            fs_inode_close(directory);
 
-            entry[i] = '\0';
-            i = 0;
+            kfree(base);
+            kfree(filename);
 
-            nextdir = fs_findentry(curdir, entry);
-
-            if (nextdir.inode == 0) {
-                if (index + 1 != len) {
-                    fs_inode_close(curdir);
-                    return -1;
-                }
-                if (!(flags & O_CREAT) || _creat(path, mode) != 0) {
-                    fs_inode_close(curdir);
-                    return -1;
-                }
-                nextdir = fs_findentry(curdir, entry);
-            }
-            fs_inode_close(curdir);
-            curdir = fs_inode_open(nextdir.inode);
+            return -1;
         }
 
+        fileEntry = fs_findentry(directory, filename);
+        if (fileEntry.inode == 0) {
+            fs_inode_close(directory);
+
+            kfree(base);
+            kfree(filename);
+
+            return -1;
+        }
     }
 
-    file_uid = curdir->data->uid;
-    file_gid = curdir->data->gid;
+    fs_inode_close(directory);
+
+    kfree(base);
+    kfree(filename);
+
+    struct fs_Inode* file = fs_inode_open(fileEntry.inode);
+    int gid, uid, file_gid, file_uid;
+
+    file_uid = file->data->uid;
+    file_gid = file->data->gid;
     gid = process->gid;
     uid = process->uid;
 
-    unsigned short perms = fs_permission(curdir);
+    unsigned short perms = fs_permission(file);
 
     if ((flags & 3) == O_RDONLY || (flags & 3) == O_RDWR) {
        if (!(perms & S_IROTH) &&
             !(perms & S_IRGRP && file_gid == gid) &&
             !(perms & S_IRUSR && file_uid == uid)) {
-            fs_inode_close(curdir);
+            fs_inode_close(file);
             return -1;
         }
     }
@@ -124,72 +128,55 @@ int _open(const char* path, int flags, int mode) {
        if(!(perms & S_IWOTH) &&
           !(perms & S_IWGRP && file_gid == gid) &&
           !(perms & S_IWUSR && file_uid == uid)) {
-            fs_inode_close(curdir);
+            fs_inode_close(file);
             return -1;
         }
     }
 
     //Pasamos los permisos
-    process->fdTable[fd] = fs_fd(curdir, flags);
-    fs_inode_close(curdir);
+    process->fdTable[fd] = fs_fd(file, flags);
+    fs_inode_close(file);
     return fd;
 }
 
 int _creat(const char* path, int mode) {
-    int index = 0;
-    int len = strlen(path);
-    int i = 0;
-    char entry[ENTRY_NAME_MAX_LEN];
-    struct fs_Inode* curdir;
-    struct fs_DirectoryEntry nextdir;
 
-    if (path[index] == '/') {
-        curdir = fs_root();
-        index++;
-    } else {
-        //TODO curdir = getcwd();
-    }
+    char* base = path_directory(path);
+    char* filename = path_file(path);
 
-    for (int i = 0; index < len; index++) {
-
-        entry[i] = path[index];
-        i++;
-
-        if (index + 1 == len) {
-            break;
-        }
-
-        if (path[index] == '/') {
-            i--;
-            entry[i] = '\0';
-            i = 0;
-
-            nextdir = fs_findentry(curdir, entry);
-
-            if (nextdir.inode == 0) {
-                return -1;
-            }
-            fs_inode_close(curdir);
-            curdir = fs_inode_open(nextdir.inode);
-        }
-
-    }
-
-    entry[i] = '\0';
-    if (fs_mknod(curdir, entry, INODE_FILE) != 0) {
+    kprintf("Trying to create %s %s\n", base, filename);
+    struct fs_Inode* directory = resolve_path(base);
+    if (directory == NULL) {
+        kfree(base);
+        kfree(filename);
         return -1;
     }
-    nextdir = fs_findentry(curdir, entry);
-    if (nextdir.inode == 0) {
+
+    if (fs_mknod(directory, filename, INODE_FILE) != 0) {
+        kfree(base);
+        kfree(filename);
+
+        fs_inode_close(directory);
         return -1;
     }
-    fs_inode_close(curdir);
-    curdir = fs_inode_open(nextdir.inode);
-    if (fs_set_permission(curdir, mode) != 0) {
-        fs_inode_close(curdir);
+
+    struct fs_DirectoryEntry fileEntry = fs_findentry(directory, filename);
+
+    fs_inode_close(directory);
+    kfree(base);
+    kfree(filename);
+
+    if (fileEntry.inode == 0) {
         return -1;
     }
-    fs_inode_close(curdir);
+
+    struct fs_Inode* file = fs_inode_open(fileEntry.inode);
+    if (fs_set_permission(file, mode) != 0) {
+        fs_inode_close(file);
+        return -1;
+    }
+    fs_inode_close(file);
+
     return 0;
 
 }
@@ -242,5 +229,97 @@ int _chdir(const char* path) {
 }
 
 int _getcwd(char* path, size_t len) {
+}
+
+struct fs_Inode* resolve_path(const char* path) {
+
+    size_t len = strlen(path);
+    size_t index = 0;
+    char entry[ENTRY_NAME_MAX_LEN];
+    struct fs_Inode* curdir;
+
+    if (path[index] == '/') {
+        curdir = fs_root();
+        index++;
+    } else {
+        curdir = resolve_path(scheduler_current()->cwd);
+        if (curdir == NULL) {
+            return NULL;
+        }
+    }
+
+    while (index < len) {
+
+        int i;
+        for (i = 0; index < len && path[index] != '/'; i++, index++) {
+            entry[i] = path[index];
+        }
+        index++;
+        entry[i] = '\0';
+
+        struct fs_DirectoryEntry nextdir = fs_findentry(curdir, entry);
+        fs_inode_close(curdir);
+
+        if (nextdir.inode == 0) {
+            return NULL;
+        }
+
+        curdir = fs_inode_open(nextdir.inode);
+    }
+
+    kprintf("Giving dir with inode %u\n", curdir->number);
+    return curdir;
+}
+
+char* path_directory(const char* path) {
+
+    size_t len = strlen(path);
+
+    // If we have a trailing slash, we ignore it
+    if (path[len - 1] == '/') {
+        len--;
+    }
+
+    // Isolate the last element so that we can ignore it
+    int lastSlash;
+    for (lastSlash = len - 1; lastSlash >= 0 && path[lastSlash] != '/'; lastSlash--);
+
+    char* result;
+    if (lastSlash == -1) {
+        // There is no directory component in this path
+        result = kalloc(sizeof(char) * 2);
+        strcpy(result, ".");
+    } else if (lastSlash == 0) {
+        // The directory component is root
+        result = kalloc(sizeof(char) * 2);
+        strcpy(result, "/");
+    } else {
+        result = kalloc(sizeof(char) * (lastSlash + 1));
+        strncpy(result, path, lastSlash);
+        result[lastSlash + 1] = 0;
+        kprintf("Path %s, lastSlash %d len %u res %s\n", path, lastSlash, len, result);
+    }
+
+    return result;
+}
+
+char* path_file(const char* path) {
+
+    size_t len = strlen(path);
+
+    // If we have a trailing slash, we ignore it
+    if (path[len - 1] == '/') {
+        len--;
+    }
+
+    // Isolate the last element so that we can ignore it
+    int lastSlash;
+    for (lastSlash = len - 1; lastSlash >= 0 && path[lastSlash] != '/'; lastSlash--);
+
+    char* result = kalloc(sizeof(char) * (len - lastSlash));
+    strncpy(result, path + (lastSlash + 1), len - lastSlash - 1);
+    result[len - lastSlash] = 0;
+
+    return result;
 }
 
