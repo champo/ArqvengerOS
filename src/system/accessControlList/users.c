5e6256ec8a0e29a750e3f0d36191a1bf348663a0
Juan Civile <elementohb@gmail.com>
2011-10-27 04:59:03 -0300

    How about we actually support finding stuff without going into infinite loops? Also, change a bit of the code that did things in a truly horrible manner

--- src/system/accessControlList/users.c
+++ src/system/accessControlList/users.c
@@ -10,14 +10,14 @@
 #define MAX_USERS       128
 
 void parseUserLine(char* str, struct User* user, char* def_group);
-int updateUserFile(struct User* user); 
+int updateUserFile(struct User* user);
 
 static struct User* registered_users[MAX_USERS];
 static int registered_users_num = 0;
 
 void users_init(void) {
 
-}    
+}
 
 
 
@@ -39,14 +39,14 @@ int get_users_num(void) {
 }
 
 struct User* get_user_by_id(int id) {
-   
+
     //return registered_users_[id];
 
     FILE* fp = fopen("/users", "r");
     disableInterrupts();
     struct User* user = kalloc(sizeof(struct User));
     enableInterrupts();
-    
+
     char str[200];
     char def_group[100];
     int first = 1;
@@ -57,17 +57,20 @@ struct User* get_user_by_id(int id) {
             //printf("%c",c);
         //}
         //printf("\n");
-        int as = fscanf(fp, "%s", str);
+        int as = fscanf(fp, "%s\n", str);
+        if (as == 0) {
+            return NULL;
+        }
         printf("str %s\n",str);
         printf("scanf devuelve %d\n",as);
         parseUserLine(str, user, def_group);
-        printf("uid %d\n", user->id);       
-        printf("gid %d\n", user->gid);       
-        printf("name %s\n", user->name);       
+        printf("uid %d\n", user->id);
+        printf("gid %d\n", user->gid);
+        printf("name %s\n", user->name);
         printf("pass %s\n", user->passwd);
         printf("group %s\n", def_group);
         //if(!first) {
-        //while(1); 
+        //while(1);
         //}
         first = 0;
     } while(user->id != id);
@@ -79,72 +82,75 @@ struct User* get_user_by_id(int id) {
 void parseUserLine(char* str, struct User* user, char* def_group) {
 
         char* nextColumn = strchr(str, ':');
+        char* buf = str;
         char aux[200];
-        
-        strncpy(user->name, str, nextColumn - str);
-        user->name[nextColumn - str] = '\0';
-        
-        strcpy(str, nextColumn + 1);
+
+        strncpy(user->name, buf, nextColumn - buf);
+        user->name[nextColumn - buf] = '\0';
+        buf = nextColumn + 1;
+
         //printf("%s\n",str);
 
-        nextColumn = strchr(str, ':');
-        strcpy(str, nextColumn + 1);
+        nextColumn = strchr(buf, ':');
+        buf = nextColumn + 1;
         //printf("%s\n",str);
-    
-        nextColumn = strchr(str, ':');
-        strncpy(aux, str, nextColumn - str);
-        aux[nextColumn - str] = '\0';
 
-        strcpy(str, nextColumn + 1);
-       
-        user->id = atoi(aux); 
+        nextColumn = strchr(buf, ':');
+        strncpy(aux, buf, nextColumn - buf);
+        aux[nextColumn - buf] = '\0';
+
+        buf = nextColumn + 1;
+
+        user->id = atoi(aux);
         //printf("uid %d\n",atoi(aux));
-    
-        nextColumn = strchr(str, ':');
-        strncpy(aux, str, nextColumn - str);
-        aux[nextColumn - str] = '\0';
 
-        strcpy(str, nextColumn + 1);
-        
+        nextColumn = strchr(buf, ':');
+        strncpy(aux, buf, nextColumn - buf);
+        aux[nextColumn - buf] = '\0';
+
+        buf = nextColumn + 1;
+
         user->gid = atoi(aux);
         //printf("gid %d\n", atoi(aux));
-    
-        nextColumn = strchr(str, ':');
-        strncpy(aux, str, nextColumn - str);
-        aux[nextColumn - str] = '\0';
+
+        nextColumn = strchr(buf, ':');
+        strncpy(aux, buf, nextColumn - buf);
+        aux[nextColumn - buf] = '\0';
 
         strcpy(user->passwd, aux);
         //printf("%s\n",aux);
 
-        strcpy(str, nextColumn + 1);
+        buf = nextColumn + 1;
 
         strcpy(def_group, aux);
 }
 
 struct User* get_user_by_name(char* name) {
-    
+
     //for (int i = 0; i < registered_users_num; i++) {
-        
+
         //if (strcmp(registered_users[i]->name, name) == 0) {
             //return registered_users[i];
         //}
     //}
     //return NULL;
-    
-    
+
+
     FILE* fp = fopen("/users", "r");
     disableInterrupts();
     struct User* user = kalloc(sizeof(struct User));
     enableInterrupts();
-    
+
     char str[200];
     char def_group[100];
     do {
-        fscanf(fp, "%s\n", str);
+        if (fscanf(fp, "%s\n", str) == 0) {
+            return NULL;
+        }
         parseUserLine(str, user, def_group);
-        //printf("uid %d\n", user->id);       
-        //printf("gid %d\n", user->gid);       
-        //printf("name %s\n", user->name);       
+        //printf("uid %d\n", user->id);
+        //printf("gid %d\n", user->gid);
+        //printf("name %s\n", user->name);
         //printf("pass %s\n", user->passwd);
         //printf("group %s\n", def_group);
 
@@ -155,18 +161,18 @@ struct User* get_user_by_name(char* name) {
 }
 
 char* get_username(int id) {
-    
+
     //return registered_users[id]->name;
     return get_user_by_id(id)->name;
 }
 
 int change_passwd(int uid, char* old_passwd, char* new_passwd) {
-    
+
     struct User* user = get_user_by_id(uid);
-     
+
     if (strcmp(user->passwd, old_passwd) == 0) {
         strcpy(user->passwd, new_passwd);
-        updateUserFile(user);    
+        updateUserFile(user);
         return PASSWD_CHANGED;
     } else {
         return INVALID_PASSWD;
@@ -184,13 +190,13 @@ int updateUserFile(struct User* user) {
         printf("%d\n",ret);
         //printf("esto es lo q hay en elarchuivo %c as\n",fgetc(fp));
     } while(ret != EOF && ret != 0);
-    
+
     for (int j= 0; j < i; j++)
-        printf("%s\n",line[j]); 
+        printf("%s\n",line[j]);
 
     fclose(fp);
     fp = fopen("/users", "w");
-    
+
 
     for (int j = 0;j < i; j++) {
         if (strncmp(user->name, line[j], strlen(user->name)) == 0) {
@@ -199,7 +205,7 @@ int updateUserFile(struct User* user) {
             fprintf(fp, line[j]);
         }
     }
-    
+
     fclose(fp);
     return 0;
 }
@@ -207,15 +213,15 @@ int updateUserFile(struct User* user) {
 int create_user(char* name, char* passwd) {
 
     if (registered_users_num == MAX_USERS) {
-        return -1; 
+        return -1;
     }
 
     struct User* new_user = kalloc(sizeof(struct User));
 
     new_user->id = registered_users_num;
-    strcpy(new_user->name, name); 
-    strcpy(new_user->passwd, passwd); 
-   
+    strcpy(new_user->name, name);
+    strcpy(new_user->passwd, passwd);
+
     registered_users[registered_users_num] = new_user;
     registered_users_num++;
 
@@ -223,7 +229,7 @@ int create_user(char* name, char* passwd) {
 }
 
 int delete_user(char* name) {
-    
+
     //TODO make this right!
     struct User* user = get_user_by_name(name);
     if (user == NULL) {
@@ -232,7 +238,7 @@ int delete_user(char* name) {
 
     registered_users[user->id] = NULL;
     registered_users_num--;
-    
+
     delete_group_member(user->gid, user->id);
     kfree(user);
 
