cbf28e8dace7befa368e75030eb7b768351f4be3
acrespo <alvaro.andres.crespo@gmail.com>
2011-10-30 15:34:54 -0300

    Removed unused commands.

--- src/shell/groupaddmem/groupaddmem.h
+++
@@ -1,8 +0,0 @@
-#ifndef __SHELL_GROUPADDMEM_GROUPADDMEM__
-#define __SHELL_GROUPADDMEM_GROUPADDMEM__ 
-
-void groupaddmem(char* argv);
-
-void manGroupaddmem(void);
-
-#endif
--- src/shell/groupdelmem/groupdelmem.h
+++
@@ -1,8 +0,0 @@
-#ifndef __SHELL_GROUPDELMEM_GROUPDELMEM__
-#define __SHELL_GROUPDELMEM_GROUPDELMEM__
-
-void groupdelmem(char* argv);
-
-void manGroupdelmem(void);
-
-#endif
