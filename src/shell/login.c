4ef25f9775c1268f0aee563cb71f61a645702ba0
acrespo <alvaro.andres.crespo@gmail.com>
2011-10-30 18:59:42 -0300

    Added busywaiting and logout command. Along with new login process father of the shell process.

---
+++ src/shell/login.c
@@ -0,0 +1,65 @@
+#include "library/stdio.h"
+#include "library/sys.h"
+#include "library/string.h"
+#include "shell/login.h"
+#include "system/accessControlList/users.h"
+#include "system/call/ioctl/keyboard.h"
+#include "shell/shell.h"
+
+#define cleanbuffer() while(getchar()!='\n')
+
+struct User* askForLogin(void);
+
+void login(char* unused) {
+
+    while(1) {
+        
+        struct User* user;
+        do {
+            printf("login:");
+        } while ((user = askForLogin()) == NULL);
+
+        setProcessPersona(getpid(), user->id, user->gid[0]);
+        
+        run(shell, NULL, 1);
+
+        wait();
+    }
+}
+
+
+struct User* askForLogin(void) {
+
+    int promptLen = strlen("login");
+    char passwd[MAX_PASSWD_LEN];
+    char username[MAX_USERNAME_LEN];
+    termios passwdTermios = {1 , 0};
+    termios userTermios = {1, 1};
+
+    scanf("%s", username);
+    cleanbuffer();
+
+    printf("Password:");
+
+    ioctl(0, TCGETS, (void*) &userTermios);
+    ioctl(0, TCSETS, (void*) &passwdTermios);
+
+    scanf("%s", passwd);
+    cleanbuffer();
+
+    ioctl(0, TCSETS, (void*) &userTermios);
+
+    printf("\n\n");
+
+    struct User* user = get_user_by_name(username);
+
+    if (user != NULL) {
+        if (strcmp(passwd, user->passwd) == 0) {
+            return user;
+        }
+    }
+    printf("Login incorrect\n");
+    return NULL;
+
+}
+
---
+++ src/shell/logout/logout.h
@@ -0,0 +1,8 @@
+#ifndef __SHELL_LOGOUT_LOGOUT__
+#define __SHELL_LOGOUT_LOGOUT__
+
+void logout(char* argv);
+
+void manLogout(void);
+
+#endif
