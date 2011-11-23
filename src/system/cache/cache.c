812fe4193724978a02841ba30751516a582a5c21
Juan Civile <elementohb@gmail.com>
2011-11-23 20:53:16 -0300

    How about making the code build
    And maybe use some of those mythical constants everybody keeps talking about

--- src/system/cache/cache.c
+++ src/system/cache/cache.c
@@ -3,47 +3,62 @@
 #include "drivers/ata.h"
 #include "library/string.h"
 
+#define CACHE_SIZE (1 << 20)
+#define SECTORS_PER_PAGE (PAGE_SIZE / SECTOR_SIZE)
+
+struct Chunk {
+    unsigned long long initialSector;
+    void* buffer;
+    int dirty;
+    int accesses;
+    int timeSinceLastAccess;
+};
+
+struct CacheTable {
+    struct Chunk chunks[100];
+    int memoryCached;
+    int chunksNum;
+};
+
 static struct CacheTable table;
+
+static struct Chunk* find_chunk(int index);
 
 int cache_read(unsigned long long sector, int count, void* buffer) {
 
     char* buff = (char*) buffer;
     struct Chunk* chunk;
-    int startBlock = sector / 8;
-    int endBlock = (sector + count) / 8;
+    int startBlock = sector / SECTORS_PER_PAGE;
+    int endBlock = (sector + count) / SECTORS_PER_PAGE;
     int counter = 0;
 
     for (int block = startBlock; block < endBlock; block++) {
-        
+
         if ((chunk = find_chunk(block)) == NULL) {
-        
-            chunk = load_chunk(block);
-            if (chunk == NULL) {
-                return -1;
-            }
+            return -1;
         }
-       
+
         chunk->accesses++;
 
         if (block == startBlock) {
 
-            int first = sector % 8;
-            int last = count > 8? 8 : count;
+            int first = sector % SECTORS_PER_PAGE;
+            int last = count > SECTORS_PER_PAGE ? SECTORS_PER_PAGE : count;
 
             memcpy(buff + (counter * SECTOR_SIZE), ((char*)chunk->buffer) + (first * SECTOR_SIZE), SECTOR_SIZE * (last - first));
             counter += (last - first);
 
         } else if (block == endBlock) {
-            
-            int last = (count - (sector % 8)) % 8;
+
+            int last = (count - (sector % SECTORS_PER_PAGE)) % SECTORS_PER_PAGE;
             memcpy(buff + (counter * SECTOR_SIZE), chunk->buffer, SECTOR_SIZE * last);
             counter += last;
         } else {
 
-            memcpy(buff + (counter * SECTOR_SIZE), chunk->buffer, SECTOR_SIZE * 8);
-            counter += 8;
+            memcpy(buff + (counter * SECTOR_SIZE), chunk->buffer, SECTOR_SIZE * SECTORS_PER_PAGE);
+            counter += SECTORS_PER_PAGE;
         }
-        
+
     }
 
     return 0;
@@ -54,43 +69,38 @@ int cache_write(unsigned long long sector, int count, void* buffer) {
 
     char* buff = (char*) buffer;
     struct Chunk* chunk;
-    int startBlock = sector / 8;
-    int endBlock = (sector + count) / 8;
+    int startBlock = sector / SECTORS_PER_PAGE;
+    int endBlock = (sector + count) / SECTORS_PER_PAGE;
     int counter = 0;
 
     for (int block = startBlock; block < endBlock; block++) {
-        
+
         if ((chunk = find_chunk(block)) == NULL) {
-        
-            chunk = load_chunk(block);
-            if (chunk == NULL) {
-                return -1;
-            }
+            return -1;
         }
 
         chunk->dirty = 1;
         chunk->accesses++;
-    
-        
+
         if (block == startBlock) {
 
-            int first = sector % 8;
-            int last = count > 8? 8 : count;
+            int first = sector % SECTORS_PER_PAGE;
+            int last = count > SECTORS_PER_PAGE ? SECTORS_PER_PAGE : count;
 
             memcpy(((char*)chunk->buffer) + first * SECTOR_SIZE, buff + (counter * SECTOR_SIZE), SECTOR_SIZE * (last - first));
             counter += (last - first);
 
         } else if (block == endBlock) {
-            
-            int last = (count - (sector % 8)) % 8;
+
+            int last = (count - (sector % SECTORS_PER_PAGE)) % SECTORS_PER_PAGE;
             memcpy(chunk->buffer, buff + (counter * SECTOR_SIZE), SECTOR_SIZE * last);
             counter += last;
         } else {
 
-            memcpy(chunk->buffer, buff + (counter * SECTOR_SIZE), SECTOR_SIZE * 8);
-            counter += 8;
+            memcpy(chunk->buffer, buff + (counter * SECTOR_SIZE), SECTOR_SIZE * SECTORS_PER_PAGE);
+            counter += SECTORS_PER_PAGE;
         }
-        
+
     }
 
     //write-trough
@@ -99,3 +109,6 @@ int cache_write(unsigned long long sector, int count, void* buffer) {
     return 0;
 }
 
+struct Chunk* find_chunk(int index) {
+    return NULL;
+}
--- src/system/cache/cache.h
+++ src/system/cache/cache.h
@@ -1,23 +1,6 @@
 #ifndef __SYSTEM_CACHE_CACHE__
 #define __SYSTEM_CACHE_CACHE__
 
-#define CACHE_SIZE      (1 << 20)
-
-struct Chunk {
-    unsigned long long initialSector;
-    void* buffer;
-    int dirty;
-    int accesses;
-    int timeSinceLastAccess;
-};
-
-struct CacheTable {
-    struct Chunk chunks[100];
-    int memoryCached;
-    int chunksNum;
-
-};
-
 int cache_read(unsigned long long sector, int count, void* buffer);
 
 int cache_write(unsigned long long sector, int count, void* buffer);
