6e5fa8116789472dd6039bb4dfce492e962a8aa5
Juan Civile <elementohb@gmail.com>
2011-11-23 23:33:13 -0300

    Force sync on reset

--- src/system/reboot.c
+++ src/system/reboot.c
@@ -1,6 +1,7 @@
 #include "system/reboot.h"
 #include "system/common.h"
 #include "system/io.h"
+#include "system/cache/cache.h"
 
 #define INTERFACE_PORT 0x64
 #define IO_PORT 0x60
@@ -22,6 +23,7 @@ void reboot(void) {
 
     // We use the keyboard controller to reset the CPU
     disableInterrupts();
+    cache_sync(1);
 
     // We need to consume everything in the keyboard buffers
     do {
