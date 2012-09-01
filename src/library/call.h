5a9330b7465614a9df39afd7b29d10b1282c8b2f
Juan Pablo Civile <elementohb@gmail.com>
2012-09-01 14:48:37 -0300

    Fix inline system_call warnings
    Having the function in the header sucks, but the warnings are a PITA.

--- src/library/call.h
+++ src/library/call.h
@@ -1,7 +1,25 @@
 #ifndef __LIBRARY_CALL__
 #define __LIBRARY_CALL__
 
-inline int system_call(int eax, int ebx, int ecx, int edx);
+/**
+ * This is the interface for the system call.
+ *
+ * @param eax, the code of the system call.
+ * @param ebx, depends on the system call.
+ * @param ecx, depends on the system call.
+ * @param edx, depends on the system call.
+ * @return depends on the system call.
+ */
+inline static int system_call(int eax, int ebx, int ecx, int edx) {
+
+    int ret;
+    __asm__ __volatile__ ( "int $0x80"
+            : "=a" (ret)
+            : "a" (eax), "b" (ebx), "c" (ecx), "d" (edx)
+    );
+
+    return ret;
+}
 
 #define SYS4(a, b, c, d) (system_call((int) (a), (int) (b), (int) (c), (int) (d)))
 #define SYS3(a, b, c) SYS4(a, b, c, 0)
